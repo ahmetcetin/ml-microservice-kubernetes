e6a8f0abe668bf5ba50ae84bf34312de80c6997b
Ahmet Cetin <ahmet@cetin@info>
2021-01-06 14:01:55 +0100

    add script to run the docker container

--- run_docker.sh
+++ run_docker.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
 
-## Complete the following steps to get Docker running locally
-
-# Step 1:
-# Build image and add a descriptive tag
-
-# Step 2: 
-# List docker images
-
-# Step 3: 
-# Run flask app
+docker build --tag=house_price_predict:latest .
+docker image ls
+docker run -p 8080:80 house_price_predict:latest
